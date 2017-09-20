c09dc5365a0c9410856f0d33d6ca69f157a66f50
Sheri R <shrdson@gmail.com>
2017-09-20 21:15:04 +0200

    Change image depending on weather condition

--- front-end-certification/local-weather/js/weather.js
+++ front-end-certification/local-weather/js/weather.js
@@ -35,6 +35,8 @@ $(function() {
       let fTemp = Math.round(cTemp * 1.8 + 32);
       let metric = "celsius";
 
+      weather.setImage(data.weather[0].id);
+
       $(".location").html(location);
       $(".description").html(descr);
       $(".temp").html(`${cTemp} <span class="metric"> °C</span>`);
@@ -49,6 +51,30 @@ $(function() {
           metric = "celsius";
         }
       });
+    },
+    setImage: function(condition) {
+      if (condition <= 200 && condition < 300) {
+        // storm
+        $(".weather-image").attr("src", "../assets/storm.jpg");
+      } else if (condition >= 300 && condition < 400) {
+        // drizzle
+        $(".weather-image").attr("src", "../assets/drizzle.jpg");
+      } else if (condition >= 500 && condition < 600) {
+        // rain
+        $(".weather-image").attr("src", "../assets/rain.jpg");
+      } else if (condition >= 600 && condition < 700) {
+        // snow
+        $(".weather-image").attr("src", "../assets/snow.jpg");
+      } else if (condition >= 700 && condition < 800) {
+        // atmosphere/fog
+        $(".weather-image").attr("src", "../assets/fog.jpg");
+      } else if (condition === 800) {
+        // clear
+        $(".weather-image").attr("src", "../assets/clear.jpg");
+      } else if (condition > 800 && condition < 900) {
+        // clouds
+        $(".weather-image").attr("src", "../assets/cloudy.jpg");
+      }
     }
   };
   weather.getLocation();
